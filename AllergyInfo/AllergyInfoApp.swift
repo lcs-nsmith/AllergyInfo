8f559bc75668252b7f7b3e4c8b6511fb2ca4ccb0
Nathan Smith <nsmith@lcs.on.ca>
2022-05-05 17:54:19 -0400

    Initial Commit

---
+++ AllergyInfo/AllergyInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllergyInfoApp.swift
+//  AllergyInfo
+//
+//  Created by Nathan Smith on 2022-05-05.
+//
+
+import SwiftUI
+
+@main
+struct AllergyInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
