e1f7b2d2154e47f9c9b86be2dd3ceea6d61847f7
Nathan Smith <nsmith@lcs.on.ca>
2022-05-12 11:40:57 -0400

    added a list item view helper

---
+++ AllergyInfo/Views/Helpers/ListItemView.swift
@@ -0,0 +1,30 @@
+//
+//  ListItemView.swift
+//  AllergyInfo
+//
+//  Created by Nathan Smith on 2022-05-12.
+//
+
+import SwiftUI
+
+struct ListItemView: View {
+    
+    let food: Food
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            
+            Text(food.title)
+            
+            RemoteImageView(fromURL: URL(string: food.image)!)
+            
+        }
+
+    }
+}
+
+struct ListItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListItemView(food: testFood)
+    }
+}
