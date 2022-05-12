a66141586ab2a2a0c6228f868bd56cbc79afe02a
Nathan Smith <nsmith@lcs.on.ca>
2022-05-12 11:40:13 -0400

    Imported remote image view from my furry friends app so that I can display images in the list from the internet

---
+++ AllergyInfo/Views/Helpers/RemoteImageView.swift
@@ -0,0 +1,42 @@
+//
+//  RemoteImageView.swift
+//  FurryFriends
+//
+//  Created by Russell Gordon on 2022-02-26.
+//
+
+import SwiftUI
+
+struct RemoteImageView: View {
+    
+    // MARK: Stored properties
+    let fromURL: URL
+    
+    // MARK: Computed properties
+    var body: some View {
+        
+        // Retrieves and displays a remote image, showing a placeholder until the image has loaded
+        AsyncImage(url: fromURL,
+                   content: { downloadedImage in
+            downloadedImage
+                .resizable()
+                .scaledToFit()
+        },
+                   placeholder: {
+            ProgressView()
+        })
+        .padding(.vertical)
+        
+    }
+    
+}
+
+struct RemoteImageView_Previews: PreviewProvider {
+    
+    // Uses a website that provides a different 640x360 image each time the address is loaded
+    static let example = URL(string: "https://picsum.photos/640/360")!
+    
+    static var previews: some View {
+        RemoteImageView(fromURL: example)
+    }
+}
